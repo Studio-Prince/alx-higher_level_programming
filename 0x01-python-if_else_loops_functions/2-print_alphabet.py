df9e6782cec0dad44c3b7d4cf56d8d71d66548ef
Studio-Prince <mosesice5@gmail.com>
2023-11-29 09:15:44 -0800

    push everything2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,6 +2,4 @@
 """Print the alphabet in lowercase, not followed by a new line."""
 
 for letter in range(97, 122):
-    if letter == ord(e) or letter == ord(q):
-        continue
     print('{}'.format(chr(letter)), end="")
